e907877f229deb344545dd44b830ca892a2537df
Rukudzo Dube <rukudihotasf@gmail.com>
2026-09-02 10:04:06 +0200

    Created variables to determine how well-known questions are.

--- Flashcards.py
+++ Flashcards.py
@@ -3,17 +3,18 @@ Question_Answer = {'Which gas moderates the ozone reaction?' : 'Nitrogen', 'What
 # Let's play a GAME!!!
 #Guess the answer to the given question. Get it right, and you move on. Get it wrong, and.... let's just say, you probably won't enjoy what comes next :)
 
+#how to sort questions based on adequacy
+
 import random
 
-#variables to tally amount of times a specific question was correct [can i pair the dictionary with a list indexing how often things are correct??]
-## {key1: item1, key2: item2, key3: item3} == [tally1, tally2, tally 3]
+tally_questions = [] 
+well_known = [] # answered correctly thrice
+medium_known = [] # answered correctly twice
+least_known = [] # answered correctly less than twice
 
-tally_questions = []
 #populate tally_questions (with 0 counter) to correspond with dictionary using a for loop
 for i in range(0, len(Question_Answer), 1):
      tally_questions.append(0)
-
-print(tally_questions)
 
 next_round = 'yes'
 score = 0
@@ -21,7 +22,7 @@ while next_round.upper() == 'YES':
 
      RandInt = random.randint(0, 3)
 
-     Q_A = list(Question_Answer.items())[RandInt] #turns dict. into list then extracts Q&A PAIR at an index in that new list
+     Q_A = list(Question_Answer.items())[RandInt] #turns dict. to select questions easily
      # Q_A = ('Question', 'Answer')
      Q = Q_A[0]
      A = Q_A[1]
@@ -30,11 +31,26 @@ while next_round.upper() == 'YES':
      if input('Answer: ', ).upper() == A.upper(): 
           print('Congratulations! You got the answer correct.')
           score += 1
+          Tally = tally_questions[RandInt] 
+          tally_questions[RandInt] = Tally + 1
           
      else:
           print('ER-OR! Wrong, wrong, wrong, better luck next time lad.')
+          Tally = tally_questions[RandInt] 
+          tally_questions[RandInt] = Tally - 1
 
      next_round = input('\nWould you like to continue to the next round? [Yes/No]', )
 
 
 print('\nThank you for practicing your work. Your score is: ', score, '\nRemember: a question a day keeps the uncertainty away!')
+for i in tally_questions:
+     if i == 3:
+          well_known.append(tally_questions.index(i))
+
+     if i == 2:
+          medium_known.append(tally_questions.index(i))
+
+     if i < 2:
+          least_known.append(tally_questions.index(i))
+
+print(well_known, ':', medium_known, ':', least_known)
