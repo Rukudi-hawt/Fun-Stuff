de560fedcd986d0fed1fac36fa40b49aec086017
Rukudzo Dube <rukudihotasf@gmail.com>
2026-09-11 09:16:45 +0200

    Began separating pairs based off of ending conjugation

--- Conjugation.py
+++ Conjugation.py
@@ -1,4 +1,5 @@
-str_verbos = '''falar, speak
+str_verbos = '''falar
+speak
 comer
 eat
 beber
@@ -6,15 +7,31 @@ drink
 durmir
 sleep
 respirar
-breathe
-gostata
-like'''
+breathe'''
 
 lst_verbos = str_verbos.splitlines()
 lst_verbos_pair = []
+lst_ar_portugues = []
+lst_er_portugues = []
+lst_ir_portugues = []
+lst_ar_translation = []
+lst_er_translation = []
+lst_ir_translation = []
 
-for i in range(0, len(lst_verbos), 1):
-     lst_verbos_pair.append(lst_verbos[i])
-     lst_verbos_pair.append(lst_verbos[i+1])
+for i in range(0, len(lst_verbos) - 1, 2):
+     lst_verbos_pair.append([lst_verbos[i], lst_verbos[i+1]])
+     if 'ar' in lst_verbos[i]:
+          lst_ar_portugues.append(lst_verbos[i])
+          lst_ar_translation.append(lst_verbos[i+1])
 
-print(lst_verbos_pair)
+     if 'er' in lst_verbos[i]:
+               lst_er_portugues.append(lst_verbos[i])
+               lst_er_translation.append(lst_verbos[i+1]) 
+
+     if 'ir' in lst_verbos[i]:
+          lst_ir_portugues.append(lst_verbos[i])   
+          lst_ir_translation.append(lst_verbos[i+1])
+
+print(lst_ar_translation, ';', lst_er_translation, ';', lst_ir_translation)
+
+
