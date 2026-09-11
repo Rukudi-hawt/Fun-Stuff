6b05e693c6c59df08133e6df0de921ca7dc00a1e
Rukudzo Dube <rukudihotasf@gmail.com>
2026-09-11 10:50:04 +0200

    Fixed issue with sorting into conjugation lists er/ir/ar

--- Conjugation.py
+++ Conjugation.py
@@ -20,18 +20,21 @@ lst_ir_translation = []
 
 for i in range(0, len(lst_verbos) - 1, 2):
      lst_verbos_pair.append([lst_verbos[i], lst_verbos[i+1]])
-     if 'ar' in lst_verbos[i]:
+
+     
+     temp_string = str(lst_verbos[i])
+     if 'ar' in temp_string[len(temp_string) - 2: len(temp_string)]:
           lst_ar_portugues.append(lst_verbos[i])
           lst_ar_translation.append(lst_verbos[i+1])
 
-     if 'er' in lst_verbos[i]:
+     if 'er' in temp_string[len(temp_string) - 2: len(temp_string)]:
                lst_er_portugues.append(lst_verbos[i])
                lst_er_translation.append(lst_verbos[i+1]) 
 
-     if 'ir' in lst_verbos[i]:
+     if 'ir' in temp_string[len(temp_string) - 2: len(temp_string)]:
           lst_ir_portugues.append(lst_verbos[i])   
           lst_ir_translation.append(lst_verbos[i+1])
 
-print(lst_ar_translation, ';', lst_er_translation, ';', lst_ir_translation)
+print(lst_er_portugues, ';', lst_ir_portugues, ';', lst_ar_portugues)
 
 
